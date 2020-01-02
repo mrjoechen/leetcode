13d529cdcd145f48252e1b0c5867e8e3813a5dc3
chenqiao <mrjoechen@qq.com>
2020-01-02 08:55:12 +0800

    twoSum

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -7,6 +7,8 @@
 
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
@@ -43,7 +45,7 @@ public class TwoSum {
      * @return
      *
      * 时间复杂度 O(n^2)
-     * 空间复杂度 O(n^2)
+     * 空间复杂度 O(1)
      */
     public static int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
@@ -51,6 +53,27 @@ public class TwoSum {
                 if (nums[j] == target - nums[i]) {
                     return new int[] { i, j };
                 }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    /**
+     * 时间复杂度 O(n)
+     * 空间复杂度 O(n)
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[] { i, map.get(complement) };
             }
         }
         throw new IllegalArgumentException("No two sum solution");

c3f861e806ffb672cff8eb000704b19257c64963
chenqiao <mrjoechen@qq.com>
2020-01-06 09:35:59 +0800

    MedianSortedArrays

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,15 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        int i = 4;
+        int j = -4;
+        System.out.println(Integer.toBinaryString(i));
+        System.out.println(Integer.toBinaryString(j));
+
+        //判断两个数的符号是否相等  ^ 异或 相同 为 0
+        System.out.println(((i >> 31) ^ (j >> 31)) == 0);
+
     }
 
 }

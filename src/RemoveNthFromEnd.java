740b281e36f89779b14d035332888b06fbc8951a
mrjctech <mrjctech@gmail.com>
2020-03-23 23:54:25 +0800

    removeNthFromEnd

---
+++ src/RemoveNthFromEnd.java
@@ -0,0 +1,63 @@
+public class RemoveNthFromEnd {
+
+
+    /**
+     * 给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
+     *
+     * 示例：
+     *
+     * 给定一个链表: 1->2->3->4->5, 和 n = 2.
+     *
+     * 当删除了倒数第二个节点后，链表变为 1->2->3->5.
+     * 说明：
+     *
+     * 给定的 n 保证是有效的。
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     *
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        ListNode listNode = new ListNode(5);
+        ListNode next = new ListNode(4);
+        listNode.next = next;
+
+
+        ListNode listNode1 = removeNthFromEnd(listNode, 1);
+        while (listNode1 != null){
+            System.out.println(listNode1.val);
+            listNode1 = listNode1.next;
+        }
+
+    }
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode first = dummy;
+        ListNode second = dummy;
+        // Advances first pointer so that the gap between first and second is n nodes apart
+        for (int i = 1; i <= n + 1; i++) {
+            first = first.next;
+        }
+        // Move first to the end, maintaining the gap
+        while (first != null) {
+            first = first.next;
+            second = second.next;
+        }
+        second.next = second.next.next;
+        return dummy.next;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+}

207535a6c2290f1e39a2c2969f08b494c761e08f
mrjctech <mrjctech@gmail.com>
2020-04-13 23:24:05 +0800

    MergeTwoLists

--- src/MergeTwoLists.java
+++ src/MergeTwoLists.java
@@ -1,5 +1,23 @@
 public class MergeTwoLists {
 
+
+    /**
+     *
+     *
+     *将两个升序链表合并为一个新的升序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+     *
+     * 示例：
+     *
+     * 输入：1->2->4, 1->3->4
+     * 输出：1->1->2->3->4->4
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/merge-two-sorted-lists
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     *
+     * @param args
+     */
     public static void main(String[] args) {
 
         ListNode listNode1 = new ListNode(1);
@@ -10,7 +28,7 @@ public class MergeTwoLists {
         listNode2.next = new ListNode(3);
         listNode2.next.next = new ListNode(4);
 
-        ListNode list = mergeTwoLists1(listNode1, listNode2);
+        ListNode list = mergeTwoLists2(listNode1, listNode2);
 
         while (list != null){
             System.out.println(list.val);
@@ -39,16 +57,31 @@ public class MergeTwoLists {
     }
 
     //迭代
-//    public static ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
-//        if (l1 == null) {
-//            return l2;
-//        }
-//        else if (l2 == null) {
-//            return l1;
-//        }
-//
-//
-//    }
+    public static ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        else if (l2 == null) {
+            return l1;
+        }
+        ListNode prehead = new ListNode(-1);
+
+        ListNode prev = prehead;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                prev.next = l1;
+                l1 = l1.next;
+            } else {
+                prev.next = l2;
+                l2 = l2.next;
+            }
+            prev = prev.next;
+        }
+
+        prev.next = l1 == null ? l2 : l1;
+
+        return prehead.next;
+    }
 
 
     public static class ListNode {
